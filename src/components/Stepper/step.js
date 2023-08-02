b6b7141fe6f26b6339cd16615ae689a26ff9e516
Raja Purushothaman <rajasignature@gmail.com>
2023-08-01 23:09:23 -0500

    Update step.js

--- src/components/Stepper/step.js
+++ src/components/Stepper/step.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const Step = (props) => {
     let classN = 'step';
-    if (props.active == 0 && props.id == 0) {
+    if (props.active === 0 && props.id == 0) {
         classN += ' selected';
-    } else if (props.active == 1 && (props.id <= 1 && props.active <= 1)) {
+    } else if (props.active === 1 && (props.id <= 1 && props.active <= 1)) {
         classN += ' selected';
-    } else if (props.active == 2 && (props.id <= 2 && props.active <= 2)) {
+    } else if (props.active === 2 && (props.id <= 2 && props.active <= 2)) {
         classN += ' selected';
-    } else if (props.active == 3 && (props.id <= 3 && props.active <= 3)) {
+    } else if (props.active === 3 && (props.id <= 3 && props.active <= 3)) {
         classN += ' selected';
     }
     let stepName = props.id + 1;
 
     const stepNav = (e) => {
         console.log(e);
-        if (e.target.classList[1] == 'selected') {
+        if (e.target.classList[1] === 'selected') {
             props.handleEvent(e.target.id);
         }
     };
